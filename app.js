nirnirs/NodeBB

commit 581e1f023eed6c6a54b266579680b55fd0306525

removed unused deps

app.js
@@ -25,13 +25,10 @@ nconf.argv().env('__');
 
 var url = require('url'),
 	async = require('async'),
-	semver = require('semver'),
 	winston = require('winston'),
-	colors = require('colors'),
 	path = require('path'),
 	pkg = require('./package.json'),
-	file = require('./src/file'),
-	utils = require('./public/src/utils.js');
+	file = require('./src/file');
 
 global.env = process.env.NODE_ENV || 'production';
 

src/meta/js.js
@@ -4,7 +4,6 @@ var winston = require('winston'),
 	fork = require('child_process').fork,
 	path = require('path'),
 	async = require('async'),
-	_ = require('underscore'),
 	nconf = require('nconf'),
 	fs = require('fs'),
 	file = require('../file'),

commit bed76420a1b49b255fe6988262b510b179cdde8c

closes #4303
Resolves regression introduced in 581e1f023eed6c6a54b266579680b55fd0306525

app.js
@@ -26,6 +26,7 @@ nconf.argv().env('__');
 var url = require('url'),
 	async = require('async'),
 	winston = require('winston'),
+	colors = require('colors'),
 	path = require('path'),
 	pkg = require('./package.json'),
 	file = require('./src/file');