nirnirs/NodeBB

commit e50522bf3cd771eb9756d7ddf9e4273c970a7802

added debug info to console on ajaxify, added throttler so requests only go through once every half second

public/src/ajaxify.js
@@ -7,6 +7,7 @@ $(document).ready(function () {
 	var location = document.location || window.location;
 	var rootUrl = location.protocol + '//' + (location.hostname || location.host) + (location.port ? ':' + location.port : '');
 	var apiXHR = null;
+	var ajaxifyTimer;
 
 	var translator;
 	var retry = true;
@@ -47,6 +48,12 @@ $(document).ready(function () {
 			};
 			$(window).on('action:reconnected', ajaxify.reconnectAction);
 		}
+
+		// Abort subsequent requests if clicked multiple times within a short window of time
+		if (ajaxifyTimer && (Date.now() - ajaxifyTimer) < 500) {
+			return true;
+		}
+		ajaxifyTimer = Date.now();
 
 		if (ajaxify.handleRedirects(url)) {
 			return true;
@@ -205,6 +212,11 @@ $(document).ready(function () {
 		$(window).trigger('action:ajaxify.contentLoaded', {url: url, tpl: tpl_url});
 
 		app.processPage();
+
+		var timeElapsed = Date.now() - ajaxifyTimer;
+		if (config.environment === 'development' && !isNaN(timeElapsed)) {
+			console.info('[ajaxify ' + url + '] Time elapsed:', timeElapsed + 'ms');
+		}
 	};
 
 	ajaxify.parseData = function () {

commit 2c260e069e4b9f24d3cb0f22fd27fe2db9f1c370

resolve regression introduced by e50522bf3cd771eb9756d7ddf9e4273c970a7802

public/src/ajaxify.js
@@ -147,6 +147,7 @@ $(document).ready(function () {
 			if (status === 403 || status === 404 || status === 500 || status === 502 || status === 503) {
 				if (status === 502 && retry) {
 					retry = false;
+					ajaxifyTimer = undefined;
 					return ajaxify.go(url, callback, quiet);
 				}
 				if (status === 502) {
@@ -167,6 +168,7 @@ $(document).ready(function () {
 				if (data.responseJSON && data.responseJSON.external) {
 					window.location.href = data.responseJSON.external;
 				} else if (typeof data.responseJSON === 'string') {
+					ajaxifyTimer = undefined;
 					ajaxify.go(data.responseJSON.slice(1), callback, quiet);
 				}
 			}