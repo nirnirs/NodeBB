nirnirs/NodeBB

commit e81de96031e1ad08a00d2225913af640a63c490e

closes #5133

src/notifications.js
@@ -464,7 +464,7 @@ var utils = require('../public/src/utils');
 							notifications[modifyIndex].bodyShort = '[[' + mergeId + '_multiple, ' + usernames[0] + ', ' + (numUsers - 1) + titleEscaped + ']]';
 						}
 
-						notifications[modifyIndex].path = set[set.length - 1].path;
+						notifications[modifyIndex].path = set[0].path;
 						break;
 
 					case 'new_register':

src/posts/delete.js
@@ -6,6 +6,7 @@ var _ = require('underscore');
 var db = require('../database');
 var topics = require('../topics');
 var user = require('../user');
+var notifications = require('../notifications');
 var plugins = require('../plugins');
 
 module.exports = function (Posts) {
@@ -126,7 +127,7 @@ module.exports = function (Posts) {
 			function (data, next) {
 				async.parallel([
 					function (next) {
-						deletePostFromTopicAndUser(pid, next);
+						deletePostFromTopicUserNotification(pid, next);
 					},
 					function (next) {
 						deletePostFromCategoryRecentPosts(pid, next);
@@ -138,18 +139,7 @@ module.exports = function (Posts) {
 						deletePostFromUsersVotes(pid, next);
 					},
 					function (next) {
-						Posts.getPostField(pid, 'toPid', function (err, toPid) {
-							if (err) {
-								return next(err);
-							}
-							if (!parseInt(toPid, 10)) {
-								return next(null);
-							}
-							async.parallel([
-								async.apply(db.sortedSetRemove, 'pid:' + toPid + ':replies', pid),
-								async.apply(db.decrObjectField, 'post:' + toPid, 'replies')
-							], next);
-						});
+						deletePostFromReplies(pid, next);
 					},
 					function (next) {
 						db.sortedSetsRemove(['posts:pid', 'posts:flagged'], pid, next);
@@ -168,54 +158,56 @@ module.exports = function (Posts) {
 		], callback);
 	};
 
-	function deletePostFromTopicAndUser(pid, callback) {
-		Posts.getPostFields(pid, ['tid', 'uid'], function (err, postData) {
-			if (err) {
-				return callback(err);
-			}
-
-			db.sortedSetsRemove([
-				'tid:' + postData.tid + ':posts',
-				'tid:' + postData.tid + ':posts:votes',
-				'uid:' + postData.uid + ':posts'
-			], pid, function (err) {
-				if (err) {
-					return callback(err);
-				}
-
-				topics.getTopicFields(postData.tid, ['tid', 'cid', 'pinned'], function (err, topicData) {
-					if (err) {
-						return callback(err);
+	function deletePostFromTopicUserNotification(pid, callback) {
+		var postData;
+		async.waterfall([
+			function (next) {
+				Posts.getPostFields(pid, ['tid', 'uid'], next);
+			},
+			function (_postData, next) {
+				postData = _postData;
+				db.sortedSetsRemove([
+					'tid:' + postData.tid + ':posts',
+					'tid:' + postData.tid + ':posts:votes',
+					'uid:' + postData.uid + ':posts'
+				], pid, next);
+			},
+			function (next) {
+				topics.getTopicFields(postData.tid, ['tid', 'cid', 'pinned'], next);
+			},
+			function (topicData, next) {
+				async.parallel([
+					function (next) {
+						db.decrObjectField('global', 'postCount', next);
+					},
+					function (next) {
+						db.decrObjectField('category:' + topicData.cid, 'post_count', next);
+					},
+					function (next) {
+						topics.decreasePostCount(postData.tid, next);
+					},
+					function (next) {
+						topics.updateTeaser(postData.tid, next);
+					},
+					function (next) {
+						updateTopicTimestamp(topicData, next);
+					},
+					function (next) {
+						db.sortedSetIncrBy('cid:' + topicData.cid + ':tids:posts', -1, postData.tid, next);
+					},
+					function (next) {
+						db.sortedSetIncrBy('tid:' + postData.tid + ':posters', -1, postData.uid, next);
+					},
+					function (next) {
+						user.incrementUserPostCountBy(postData.uid, -1, next);
+					},
+					function (next) {
+						notifications.rescind('new_post:tid:' + postData.tid + ':pid:' + pid + ':uid:' + postData.uid, next);
 					}
-
-					async.parallel([
-						function (next) {
-							db.decrObjectField('global', 'postCount', next);
-						},
-						function (next) {
-							db.decrObjectField('category:' + topicData.cid, 'post_count', next);
-						},
-						function (next) {
-							topics.decreasePostCount(postData.tid, next);
-						},
-						function (next) {
-							topics.updateTeaser(postData.tid, next);
-						},
-						function (next) {
-							updateTopicTimestamp(topicData, next);
-						},
-						function (next) {
-							db.sortedSetIncrBy('cid:' + topicData.cid + ':tids:posts', -1, postData.tid, next);
-						},
-						function (next) {
-							db.sortedSetIncrBy('tid:' + postData.tid + ':posters', -1, postData.uid, next);
-						},
-						function (next) {
-							user.incrementUserPostCountBy(postData.uid, -1, next);
-						}
-					], callback);
-				});
-			});
+				], next);
+			}
+		], function (err) {
+			callback(err);
 		});
 	}
 
@@ -287,5 +279,21 @@ module.exports = function (Posts) {
 			], callback);
 		});
 	}
+
+	function deletePostFromReplies(pid, callback) {
+		Posts.getPostField(pid, 'toPid', function (err, toPid) {
+			if (err) {
+				return callback(err);
+			}
+			if (!parseInt(toPid, 10)) {
+				return callback(null);
+			}
+			async.parallel([
+				async.apply(db.sortedSetRemove, 'pid:' + toPid + ':replies', pid),
+				async.apply(db.decrObjectField, 'post:' + toPid, 'replies')
+			], callback);
+		});
+	}
+
 
 };

commit e1849f38dcc9d768376befdc41e1384a04479dc6

Fix followed topic notifications going to the last unread post
Regression introduced in e81de96031e1ad08a00d2225913af640a63c490e.

Bug originally reported in #4469.

public/src/app.js
@@ -631,12 +631,12 @@ app.cacheBuster = null;
 			return;
 		}
 
-		templates.parse('partials/cookie-consent', config.cookies, function(html) {
+		templates.parse('partials/cookie-consent', config.cookies, function (html) {
 			$(document.body).append(html);
 
 			var warningEl = $('.cookie-consent');
 			var dismissEl = warningEl.find('button');
-			dismissEl.on('click', function() {
+			dismissEl.on('click', function () {
 				// Save consent cookie and remove warning element
 				var now = new Date();
 				window.localStorage.setItem('cookieconsent', '1');

src/notifications.js
@@ -464,7 +464,7 @@ var utils = require('../public/src/utils');
 							notifications[modifyIndex].bodyShort = '[[' + mergeId + '_multiple, ' + usernames[0] + ', ' + (numUsers - 1) + titleEscaped + ']]';
 						}
 
-						notifications[modifyIndex].path = set[0].path;
+						notifications[modifyIndex].path = set[set.length - 1].path;
 						break;
 
 					case 'new_register':

test/notifications.js
@@ -3,6 +3,7 @@
 
 
 var assert = require('assert');
+var async = require('async');
 
 var db = require('./mocks/databasemock');
 var user = require('../src/user');
@@ -93,6 +94,76 @@ describe('Notifications', function () {
 					done();
 				});
 			});
+		});
+	});
+
+	it('should link to the first unread post in a watched topic', function (done) {
+		var categories = require('../src/categories');
+		var topics = require('../src/topics');
+
+		var watcherUid;
+		var cid;
+		var tid;
+		var pid;
+
+		async.waterfall([
+			function (next) {
+				user.create({username: 'watcher'}, next);
+			},
+			function (_watcherUid, next) {
+				watcherUid = _watcherUid;
+
+				categories.create({
+					name: 'Test Category',
+					description: 'Test category created by testing script'
+				}, next);
+			},
+			function (category, next) {
+				cid = category.cid;
+
+				topics.post({
+					uid: watcherUid,
+					cid: cid,
+					title: 'Test Topic Title',
+					content: 'The content of test topic'
+				}, next);
+			},
+			function (topic, next) {
+				tid = topic.topicData.tid;
+
+				topics.follow(tid, watcherUid, next);
+			},
+			function (next) {
+				topics.reply({
+					uid: uid,
+					content: 'This is the first reply.',
+					tid: tid
+				}, next);
+			},
+			function (post, next) {
+				pid = post.pid;
+
+				topics.reply({
+					uid: uid,
+					content: 'This is the second reply.',
+					tid: tid
+				}, next);
+			},
+			function (post, next) {
+				// notifications are sent asynchronously with a 1 second delay.
+				setTimeout(next, 3000);
+			},
+			function (next) {
+				user.notifications.get(watcherUid, next);
+			},
+			function (notifications, next) {
+				assert.equal(notifications.unread.length, 1, 'there should be 1 unread notification');
+				assert.equal('/post/' + pid, notifications.unread[0].path, 'the notification should link to the first unread post');
+				next();
+			}
+		], function (err) {
+			assert.ifError(err);
+			done();
 		});
 	});
 