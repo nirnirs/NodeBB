nirnirs/NodeBB

commit 0053b662e375ad20dafe95511c2ac5b4a1afd0d9

trimming composer input before doing length check in replies

src/topics/create.js
@@ -3,6 +3,7 @@
 
 var async = require('async');
 var validator = require('validator');
+var S = require('string');
 var db = require('../database');
 var utils = require('../../public/src/utils');
 var plugins = require('../plugins');
@@ -327,6 +328,9 @@ module.exports = function (Topics) {
 	}
 
 	function check(item, min, max, minError, maxError, callback) {
+		// Trim and remove HTML (latter for composers that send in HTML, like redactor)
+		item = S(item.trim()).stripTags().s;
+
 		if (!item || item.length < parseInt(min, 10)) {
 			return callback(new Error('[[error:' + minError + ', ' + min + ']]'));
 		} else if (item.length > parseInt(max, 10)) {

commit b2a68f0a01842dd41e78e12c0f81db5d51d6da53

fix topic creation regression caused by 0053b662e375ad20dafe95511c2ac5b4a1afd0d9

src/topics/create.js
@@ -329,7 +329,9 @@ module.exports = function (Topics) {
 
 	function check(item, min, max, minError, maxError, callback) {
 		// Trim and remove HTML (latter for composers that send in HTML, like redactor)
-		item = S(item.trim()).stripTags().s;
+		if (typeof item === 'string') {
+			item = S(item.trim()).stripTags().s;
+		}
 
 		if (!item || item.length < parseInt(min, 10)) {
 			return callback(new Error('[[error:' + minError + ', ' + min + ']]'));