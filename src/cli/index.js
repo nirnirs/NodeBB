nirnirs/NodeBB

commit 6bfb1b1fdeafa8763e1d37758bfc80b2c34dde72

updated upgrade logic to not break ACP restart flow

src/cli/package-install.js
@@ -30,7 +30,7 @@ function updatePackageFile() {
 exports.updatePackageFile = updatePackageFile;
 
 function installAll() {
-	process.stdout.write('\n');
+	process.stdout.write('  started\n'.green);
 
 	var prod = global.env !== 'development';
 	var command = 'npm install';

src/cli/upgrade.js
@@ -16,6 +16,7 @@ var steps = {
 		handler: function (next) {
 			packageInstall.updatePackageFile();
 			packageInstall.preserveExtraneousPlugins();
+			process.stdout.write('  OK\n'.green);
 			next();
 		},
 	},
@@ -54,11 +55,8 @@ function runSteps(tasks) {
 	tasks = tasks.map(function (key, i) {
 		return function (next) {
 			process.stdout.write('\n' + ((i + 1) + '. ').bold + steps[key].message.yellow);
-			return steps[key].handler(function (err, inhibitOk) {
+			return steps[key].handler(function (err) {
 				if (err) { return next(err); }
-				if (!inhibitOk) {
-					process.stdout.write('  OK'.green + '\n'.reset);
-				}
 				next();
 			});
 		};

src/cli/upgrade-plugins.js
@@ -212,7 +212,7 @@ function upgradePlugins(callback) {
 				});
 			} else {
 				console.log('Package upgrades skipped'.yellow + '. Check for upgrades at any time by running "'.reset + './nodebb upgrade -p'.green + '".'.reset);
-				callback(null, true);
+				callback();
 			}
 		});
 	});

src/meta/build.js
@@ -212,7 +212,7 @@ function build(targets, callback) {
 		}
 
 		winston.info('[build] Asset compilation successful. Completed in ' + totalTime + 'sec.');
-		callback(null, true);
+		callback();
 	});
 }
 

commit 427654455711c8353a51cc1d613efa1a915a0f04

Ensure installed correct version of modules on startup, bump dependencies (#6207)
* Ensure installed correct version of modules

* Bump dependencies

* Bump promise-polyfill

* Fix emailer test

* Fix auto-install regression

introduced in 6bfb1b1fdeafa8763e1d37758bfc80b2c34dde72

src/cli/index.js
@@ -6,9 +6,11 @@ var path = require('path');
 var packageInstall = require('./package-install');
 var dirname = require('./paths').baseDir;
 
+var defaultPackage;
+
 // check to make sure dependencies are installed
 try {
-	fs.readFileSync(path.join(dirname, 'package.json'));
+	defaultPackage = JSON.parse(fs.readFileSync(path.join(dirname, 'install/package.json'), 'utf8'));
 } catch (e) {
 	if (e.code === 'ENOENT') {
 		console.warn('package.json not found.');
@@ -29,13 +31,24 @@ try {
 }
 
 try {
-	fs.readFileSync(path.join(dirname, 'node_modules/async/package.json'), 'utf8');
-	fs.readFileSync(path.join(dirname, 'node_modules/commander/package.json'), 'utf8');
-	fs.readFileSync(path.join(dirname, 'node_modules/colors/package.json'), 'utf8');
-	fs.readFileSync(path.join(dirname, 'node_modules/nconf/package.json'), 'utf8');
+	var semver = require('semver');
+
+	var checkVersion = function (packageName) {
+		var version = JSON.parse(fs.readFileSync(path.join(dirname, 'node_modules', packageName, 'package.json'), 'utf8')).version;
+		if (!semver.satisfies(version, defaultPackage.dependencies[packageName])) {
+			var e = new TypeError('Incorrect dependency version: ' + packageName);
+			e.code = 'DEP_WRONG_VERSION';
+			throw e;
+		}
+	};
+
+	checkVersion('nconf');
+	checkVersion('async');
+	checkVersion('commander');
+	checkVersion('colors');
 } catch (e) {
-	if (e.code === 'ENOENT') {
-		console.warn('Dependencies not yet installed.');
+	if (['ENOENT', 'DEP_WRONG_VERSION', 'MODULE_NOT_FOUND'].indexOf(e.code) !== -1) {
+		console.warn('Dependencies outdated or not yet installed.');
 		console.log('Installing them now...\n');
 
 		packageInstall.installAll();

src/cli/package-install.js
@@ -30,8 +30,6 @@ function updatePackageFile() {
 exports.updatePackageFile = updatePackageFile;
 
 function installAll() {
-	process.stdout.write('  started\n'.green);
-
 	var prod = global.env !== 'development';
 	var command = 'npm install';
 	try {

src/cli/upgrade.js
@@ -23,6 +23,7 @@ var steps = {
 	install: {
 		message: 'Bringing base dependencies up to date...',
 		handler: function (next) {
+			process.stdout.write('  started\n'.green);
 			packageInstall.installAll();
 			next();
 		},

src/meta/js.js
@@ -14,6 +14,7 @@ var JS = module.exports;
 
 JS.scripts = {
 	base: [
+		'node_modules/promise-polyfill/dist/polyfill.js',
 		'node_modules/jquery/dist/jquery.js',
 		'node_modules/socket.io-client/dist/socket.io.js',
 		'public/vendor/jquery/timeago/jquery.timeago.js',
@@ -36,7 +37,6 @@ JS.scripts = {
 		'public/src/ajaxify.js',
 		'public/src/overrides.js',
 		'public/src/widgets.js',
-		'node_modules/promise-polyfill/promise.js',
 	],
 
 	// files listed below are only available client-side, or are bundled in to reduce # of network requests on cold load

test/emailer.js
@@ -112,6 +112,7 @@ describe('emailer', function () {
 		Meta.configs.setMultiple({
 			'email:smtpTransport:enabled': '1',
 			'email:smtpTransport:user': username,
+			'email:smtpTransport:pass': 'anything',
 			'email:smtpTransport:service': 'nodebb-custom-smtp',
 			'email:smtpTransport:port': 4000,
 			'email:smtpTransport:host': 'localhost',