nirnirs/NodeBB

commit 17b38263adc75f2f688c5ff567a289633997b6ff

fix: escape bootswatchSkin and homepageRoute

src/user/settings.js
@@ -1,6 +1,8 @@
 
 'use strict';
 
+const validator = require('validator');
+
 const meta = require('../meta');
 const db = require('../database');
 const plugins = require('../plugins');
@@ -56,7 +58,8 @@ module.exports = function (User) {
 		settings.upvoteNotifFreq = getSetting(settings, 'upvoteNotifFreq', 'all');
 		settings.restrictChat = parseInt(getSetting(settings, 'restrictChat', 0), 10) === 1;
 		settings.topicSearchEnabled = parseInt(getSetting(settings, 'topicSearchEnabled', 0), 10) === 1;
-		settings.bootswatchSkin = settings.bootswatchSkin || '';
+		settings.bootswatchSkin = validator.escape(String(settings.bootswatchSkin || ''));
+		settings.homePageRoute = validator.escape(String(settings.homePageRoute || ''));
 		settings.scrollToMyPost = parseInt(getSetting(settings, 'scrollToMyPost', 1), 10) === 1;
 		settings.categoryWatchState = getSetting(settings, 'categoryWatchState', 'notwatching');
 

commit 0a1d1bc2accd2424cf9327b83820e567b617854c

fix: #8168 re-allowing slashes in homePageRoute
Resolves regression introduced by 17b38263adc75f2f688c5ff567a289633997b6ff

src/user/settings.js
@@ -59,7 +59,7 @@ module.exports = function (User) {
 		settings.restrictChat = parseInt(getSetting(settings, 'restrictChat', 0), 10) === 1;
 		settings.topicSearchEnabled = parseInt(getSetting(settings, 'topicSearchEnabled', 0), 10) === 1;
 		settings.bootswatchSkin = validator.escape(String(settings.bootswatchSkin || ''));
-		settings.homePageRoute = validator.escape(String(settings.homePageRoute || ''));
+		settings.homePageRoute = validator.escape(String(settings.homePageRoute || '')).replace('&#x2F;', '/');
 		settings.scrollToMyPost = parseInt(getSetting(settings, 'scrollToMyPost', 1), 10) === 1;
 		settings.categoryWatchState = getSetting(settings, 'categoryWatchState', 'notwatching');
 