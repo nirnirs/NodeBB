nirnirs/NodeBB

commit 26fbd904f31d83e5f46f6e32df51251b74c327c8

fix: register

src/middleware/headers.js
@@ -56,7 +56,7 @@ module.exports = function (middleware) {
 		}
 
 		// Validate session
-		if (!req.session.meta && !res.get('Set-Cookie')) {
+		if (req.uid && !req.session.meta && !res.get('Set-Cookie')) {
 			res.clearCookie(nconf.get('sessionKey'), meta.configs.cookie.get());
 		}
 

commit c4434a0146919455b76e58b28b3414e05de0ac05

fix: #8142 invalid session warning if server-side session destroyed
Resolved regression caused by 26fbd904f31d83e5f46f6e32df51251b74c327c8

src/middleware/headers.js
@@ -56,8 +56,9 @@ module.exports = function (middleware) {
 		}
 
 		// Ensure that the session is valid. This block guards against edge-cases where the server-side session has
-		// been deleted (but client-side cookie still exists)
-		if (req.uid > 0 && !req.session.meta && !res.get('Set-Cookie')) {
+		// been deleted (but client-side cookie still exists).
+		// req.session.flash is present if you visit register/login, so all logged-in users have it, but it is missing if your server-side session got destroyed.
+		if (!req.session.flash && !req.session.meta && !res.get('Set-Cookie')) {
 			res.clearCookie(nconf.get('sessionKey'), meta.configs.cookie.get());
 		}
 