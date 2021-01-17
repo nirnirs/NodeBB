nirnirs/NodeBB

commit 0c19affc9746ec10d31000d9befbf4f7ddd3ab67

fix: access checks for tags and thumbs get route

src/controllers/write/topics.js
@@ -86,16 +86,28 @@ Topics.unfollow = async (req, res) => {
 };
 
 Topics.addTags = async (req, res) => {
+	if (!await privileges.topics.canEdit(req.params.tid, req.user.uid)) {
+		return helpers.formatApiResponse(403, res);
+	}
+
 	await topics.createTags(req.body.tags, req.params.tid, Date.now());
 	helpers.formatApiResponse(200, res);
 };
 
 Topics.deleteTags = async (req, res) => {
+	if (!await privileges.topics.canEdit(req.params.tid, req.user.uid)) {
+		return helpers.formatApiResponse(403, res);
+	}
+
 	await topics.deleteTopicTags(req.params.tid);
 	helpers.formatApiResponse(200, res);
 };
 
 Topics.getThumbs = async (req, res) => {
+	if (!await privileges.topics.can('topics:read', req.params.tid, req.uid)) {
+		return helpers.formatApiResponse(403, res);
+	}
+
 	helpers.formatApiResponse(200, res, await topics.thumbs.get(req.params.tid));
 };
 

src/routes/write/topics.js
@@ -35,7 +35,7 @@ module.exports = function () {
 	setupApiRoute(router, 'put', '/:tid/tags', [...middlewares, middleware.checkRequired.bind(null, ['tags']), middleware.assert.topic], controllers.write.topics.addTags);
 	setupApiRoute(router, 'delete', '/:tid/tags', [...middlewares, middleware.assert.topic], controllers.write.topics.deleteTags);
 
-	setupApiRoute(router, 'get', '/:tid/thumbs', [], controllers.write.topics.getThumbs);
+	setupApiRoute(router, 'get', '/:tid/thumbs', middleware.authenticateOrGuest, controllers.write.topics.getThumbs);
 	setupApiRoute(router, 'post', '/:tid/thumbs', [multipartMiddleware, middleware.validateFiles, ...middlewares], controllers.write.topics.addThumb);
 	setupApiRoute(router, 'put', '/:tid/thumbs', [], controllers.write.topics.migrateThumbs);
 	setupApiRoute(router, 'delete', '/:tid/thumbs', [...middlewares, middleware.checkRequired.bind(null, ['path'])], controllers.write.topics.deleteThumb);

commit 96176a8b8c817d21a6c9daab93bde49327a4575c

fix: regression caused by 0c19affc9746ec10d31000d9befbf4f7ddd3ab67
Access checks were added for topic GET route, but occasionally a post_uuid is passed in, which is available to everyone, and so checks should be skipped

src/controllers/write/topics.js
@@ -104,8 +104,14 @@ Topics.deleteTags = async (req, res) => {
 };
 
 Topics.getThumbs = async (req, res) => {
-	if (!await privileges.topics.can('topics:read', req.params.tid, req.uid)) {
-		return helpers.formatApiResponse(403, res);
+	if (isFinite(req.params.tid)) {	// post_uuids can be passed in occasionally, in that case no checks are necessary
+		const [exists, canRead] = await Promise.all([
+			topics.exists(req.params.tid),
+			privileges.topics.can('topics:read', req.params.tid, req.uid),
+		]);
+		if (!exists || !canRead) {
+			return helpers.formatApiResponse(403, res);
+		}
 	}
 
 	helpers.formatApiResponse(200, res, await topics.thumbs.get(req.params.tid));