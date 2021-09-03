nirnirs/NodeBB

commit fb7844f0e3981dd3f9d0f9e92e78cfb5b349b5ab

feat: removed registerAndLoginUserCallback local helper, added handling if a bad interstitial doesn't go away nor throw errors

src/controllers/authentication.js
@@ -73,9 +73,6 @@ async function registerAndLoginUser(req, res, userData) {
 	return complete;
 }
 
-const registerAndLoginUserCallback = util.callbackify(registerAndLoginUser);
-
-
 authenticationController.register = async function (req, res) {
 	const registrationType = meta.config.registrationType || 'normal';
 
@@ -197,7 +194,12 @@ authenticationController.registerComplete = function (req, res, next) {
 
 		if (req.session.registration.register === true) {
 			res.locals.processLogin = true;
-			registerAndLoginUserCallback(req, res, req.session.registration, done);
+			req.body.noscript = 'true';	// trigger full page load on error
+
+			const data = await registerAndLoginUser(req, res, req.session.registration);
+			if (!data) {
+				winston.warn('[register] Interstitial callbacks processed with no errors, but one or more interstitials remain. This is likely an issue with one of the interstitials not properly handling a null case or invalid value.');
+			}
 		} else {
 			// Update user hash, clear registration data in session
 			const payload = req.session.registration;

commit 1c5539f40a8d216b0e429a3b0229b24c702a57ad

fix: #9772, regression from https://github.com/NodeBB/NodeBB/commit/fb7844f0e3981dd3f9d0f9e92e78cfb5b349b5ab

src/controllers/authentication.js
@@ -200,7 +200,7 @@ authenticationController.registerComplete = function (req, res, next) {
 				return winston.warn('[register] Interstitial callbacks processed with no errors, but one or more interstitials remain. This is likely an issue with one of the interstitials not properly handling a null case or invalid value.');
 			}
 
-			done();
+			done(null, data);
 		} else {
 			// Update user hash, clear registration data in session
 			const payload = req.session.registration;